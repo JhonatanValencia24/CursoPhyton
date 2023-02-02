12e9fea276fb09b943cdd2b093a81000d29e0ad6
JhonatanValencia24 <124216410+JhonatanValencia24@users.noreply.github.com>
2023-02-01 20:51:17 -0500

    Add files via upload

---
+++ Ejer1parte2.py
@@ -0,0 +1,31 @@
+print('\nPrograma que identifica el tipo de dato de un valor ingresado por el usuario, se realizarán cinco interacciones:')
+print('\nPrimera Interacción, ingrese un valor cualquiera:')
+#la funcion input permite leer lo que se ingrese por teclado.
+# y lo guardo en la variable lectura
+# con la funcion type(),identificamos el tipo de dato ingresado
+# mostramos el tipo de dato anidando con la funcion print
+lectura=input('Ingreso 1:')
+print('Este tipo de dato en Python es:')
+print(type(lectura))
+
+print('\nSegunda Interacción, ingrese un valor cualquiera:')
+lectura=input('Ingreso 2:')
+print('Este tipo de dato en Python es:')
+print(type(lectura))
+
+print('\nTercera  Interacción, ingrese un valor cualquiera:')
+lectura=input('Ingreso 2:')
+print('Este tipo de dato en Python es:')
+print(type(lectura))
+
+print('\nCuarta  Interacción, ingrese un valor cualquiera:')
+lectura=input('Ingreso 2:')
+print('Este tipo de dato en Python es:')
+print(type(lectura))
+
+print('\nQuinta Interacción, ingrese un valor cualquiera:')
+lectura=input('Ingreso 2:')
+print('Este tipo de dato en Python es:')
+print(type(lectura))
+
+print("¡YA NO SE HARÁN MÁS INTERACCIONES!")
